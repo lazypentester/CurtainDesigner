7110891461475d638e28d6c6af577715f7d0da15
Gleb <gobubov.gleb@ukr.net>
2020-07-09 00:06:45 +0300

    начальный этап дизайна главной формы

--- CurtainDesigner/Form1.Designer.cs
+++ CurtainDesigner/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.panelSubMenu = new System.Windows.Forms.Panel();
+            this.panelSideSubMenu = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelMainMenu
+            // 
+            this.panelMainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMainMenu.Name = "panelMainMenu";
+            this.panelMainMenu.Size = new System.Drawing.Size(934, 59);
+            this.panelMainMenu.TabIndex = 0;
+            // 
+            // panelSubMenu
+            // 
+            this.panelSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(94)))), ((int)(((byte)(95)))));
+            this.panelSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSubMenu.Location = new System.Drawing.Point(0, 59);
+            this.panelSubMenu.Name = "panelSubMenu";
+            this.panelSubMenu.Size = new System.Drawing.Size(934, 91);
+            this.panelSubMenu.TabIndex = 1;
+            // 
+            // panelSideSubMenu
+            // 
+            this.panelSideSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
+            this.panelSideSubMenu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelSideSubMenu.Location = new System.Drawing.Point(636, 150);
+            this.panelSideSubMenu.Name = "panelSideSubMenu";
+            this.panelSideSubMenu.Size = new System.Drawing.Size(298, 411);
+            this.panelSideSubMenu.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.panelSideSubMenu);
+            this.Controls.Add(this.panelSubMenu);
+            this.Controls.Add(this.panelMainMenu);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMainMenu;
+        private System.Windows.Forms.Panel panelSubMenu;
+        private System.Windows.Forms.Panel panelSideSubMenu;
     }
 }
 
